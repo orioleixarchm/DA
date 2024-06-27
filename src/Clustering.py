64fb1dff32b365837ff4140c4f87354fff7dc60f
orioleixarchm <133284998+orioleixarchm@users.noreply.github.com>
2024-06-27 13:31:48 +0200

    Update Clustering.py

--- src/Clustering.py
+++ src/Clustering.py
@@ -16,14 +16,15 @@ links = [
 ]
 
 #Downloading data
+dir = os.getcwd()
 file_ids = [link.split('/d/')[1].split('/')[0] for link in links]
 urls = [f'https://drive.google.com/uc?id={file_id}' for file_id in file_ids]
-gdown.download(urls[0], 'interventions.xlsx', quiet=False)
-gdown.download(urls[1], 'ambulances.xlsx', quiet=False)
-gdown.download(urls[2], 'aed.xlsx', quiet=False)
-interventions = pd.read_excel(f'interventions.xlsx')
-ambulances = pd.read_excel(f'ambulances.xlsx')
-aed = pd.read_excel(f'aed.xlsx')
+gdown.download(urls[0], os.path.join(dir,'interventions.xlsx'), quiet=False)
+gdown.download(urls[1], os.path.join(dir,'ambulances.xlsx'), quiet=False)
+gdown.download(urls[2], os.path.join(dir,'aed.xlsx'), quiet=False)
+interventions = pd.read_excel(os.path.join(dir,'interventions.xlsx'))
+ambulances = pd.read_excel(os.path.join(dir,'ambulances.xlsx'))
+aed = pd.read_excel(os.path.join(dir,'aed.xlsx'))
 
 #Loading AED data (locally)
 #dir = os.getcwd()
@@ -72,12 +73,9 @@ print('\nTotal number of clusters:\n',len(bluespots['Cluster'].value_counts()))
 
 #Exporting to excel
 dir = os.getcwd()
-hotspotspath = os.path.join(dir,'hotspots.xlsx')
-greenspotspath = os.path.join(dir,'greenspots.xlsx')
-bluespotspath = os.path.join(dir,'bluespots.xlsx')
-interventions.to_excel(hotspotspath, index=False)
-aed.to_excel(greenspotspath, index=False)
-ambulances.to_excel(bluespotspath, index=False)
+interventions.to_excel(os.path.join(dir,'hotspots.xlsx'), index=False)
+aed.to_excel(os.path.join(dir,'greenspots.xlsx'), index=False)
+ambulances.to_excel(os.path.join(dir,'bluespots.xlsx'), index=False)
 
 
 
