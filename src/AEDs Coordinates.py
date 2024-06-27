263bea28ebc6d119ed3a3c74214379c884fe36f8
orioleixarchm <133284998+orioleixarchm@users.noreply.github.com>
2024-06-27 12:16:32 +0200

    Update AEDs Coordinates.py

--- src/AEDs Coordinates.py
+++ src/AEDs Coordinates.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import gdown
+import os
 from geopy.geocoders import Nominatim
 from utilities import geocode_address
 
@@ -20,7 +21,14 @@ aed = pd.read_parquet(f'aed_locations.parquet.gzip')
 aed = aed.dropna(subset=['address','number','postal_code'])
 aed[['number','postal_code']] = aed[['number','postal_code']].astype(int).astype(str)
 aed['Full_adress'] = aed.apply(lambda row: f"{row['address']} {row['number']}, {row['postal_code']}", axis=1)
-outpath='C:/Users/oriol/OneDrive/UNI/MASTER/Modern Data Analytics/Project/data/' #New path needs to be specified
-aed['Full_adress'].to_csv(f'{outpath}adresses.csv', index=False)
+
+#Acquiring coordinates
+dir = os.getcwd()
+addresspath = os.path.join(dir,'adresses.csv')
+aed['Full_adress'].to_csv(addresspath, index=False)
 aed[['latitude', 'longitude']] = aed.apply(geocode_address, axis=1)
 print(aed.head())
+
+#Export to excel
+aedpath = os.path.join(dir,'aed_coord.xlsx')
+aed.to_excel(aedpath, index=False)
