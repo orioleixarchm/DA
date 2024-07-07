8f7891328d7bf3255e41c72a03dd476baf120286
orioleixarchm <133284998+orioleixarchm@users.noreply.github.com>
2024-07-07 21:08:23 +0200

    Update utilities.py

--- src/utilities.py
+++ src/utilities.py
@@ -1,38 +1,37 @@
 """
 Utility functions for the project.
 
-This module contains the functions mainly used for coordinate and distance computations,
-as well as the ones used for visualization at the app development within the project.
+This module contains reusable functions data processing,
+and visualization used across different parts of the project.
 """
 
 #Loading Packages
 import pandas as pd
+import numpy as np
 from geopy.distance import geodesic
+import streamlit as st
 import folium
 
-#Returns the coordinates for a given full address.
+#Reusable functions
 def geocode_address(row):
     location = geolocator.geocode(row['Full_adress'])
     if location:
         return pd.Series([location.latitude, location.longitude])
     else:
         return pd.Series([None, None])
-        
-#Identifies the closest aed to a given set fo coordinates and returns the distance in meters to it from that given coordinates.   
+    
 def distance_interv_aed(row,aed_tree,aed_matrix):
     dist, idx = aed_tree.query([row['Latitude'], row['Longitude']])
     nearest_aed = aed_matrix[idx]
     distance_aed = geodesic((row['Latitude'], row['Longitude']), nearest_aed).meters
     return distance_aed
 
-#Identifies the closest ambulance center to a given set fo coordinates and returns the distance in meters to it from that given coordinates.   
 def distance_interv_amb(row,amb_tree,ambulances_matrix):
     dist, idx = amb_tree.query([row['Latitude'], row['Longitude']])
     nearest_amb = ambulances_matrix[idx]
     distance_amb = geodesic((row['Latitude'], row['Longitude']), nearest_amb).meters
     return distance_amb 
 
-#Adds location markers (specifying the kind of symbol and color displayed) at given coordinates in a map.   
 def add_marker(row, map_obj, color, icon, popup_text):
     folium.Marker(
         location=[row['Latitude'], row['Longitude']],
@@ -40,8 +39,7 @@ def add_marker(row, map_obj, color, icon, popup_text):
         icon=folium.Icon(color=color, icon=icon)
     ).add_to(map_obj)
 
-#Adds circular markers (specifying the kind of symbol and color displayed) at given coordinates in a map.   
-def add_circle_marker(row, map_obj, color):
+def add_circle_marker(row, map_obj,color):
     folium.CircleMarker(
         location=[row['Latitude'], row['Longitude']],
         radius=5,
